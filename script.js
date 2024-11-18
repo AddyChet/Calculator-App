a9e3c53de38055a5f59a0ed3d8cbd3869d50d125
AddyChet <adarshbhandari1002@gmail.com>
2024-11-18 10:11:08 +0530

    calculator

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const numBtn = document.querySelectorAll(".btn-num");
 const input = document.getElementById("input");
 
-
 function updateInput(value) {
-    if (input.value === "Error") {
-        input.value = ""; 
-    }
-    input.value += value;
+  if (input.value === "Error") {
+    input.value = "";
+  }
+  input.value += value;
 }
 
-numBtn.forEach(button => {
-    button.addEventListener("click", () => {
-        //When clicked, values inside input bar will clear
-        if (button.innerText === "AC") {
-            input.value = "";
-        } 
-        //when clicked, values will be deleted starting from end
-        else if (button.innerText === "DEL") {
-            input.value = input.value.slice(0, -1);
-        } 
-        else if (button.innerText !== "=") {
-            //updating input and clearing if there is Error text
-            updateInput(button.innerText);
-            
-        } 
-        //setting a condition for "=" so that it will evaluate instead of being shown in input bar
-        else {
-            try {
-                input.value = eval(input.value);
-            } catch (error) {
-                input.value = "Error";
-            }
+numBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    //When clicked, values inside input bar will clear
+    if (button.innerText === "AC") {
+      input.value = "";
+    }
+    //when clicked, values will be deleted starting from end
+    else if (button.innerText === "DEL") {
+      input.value = input.value.slice(0, -1);
+    } else if (button.innerText !== "=") {
+      //updating input and clearing if there is Error text
+      updateInput(button.innerText);
+    }
+    //setting a condition for "=" so that it will evaluate instead of being shown in input bar
+    else {
+      try {
+        // Using a safer alternative to evaluate the expression, instead of using eval()
+        if (!input.value.includes("%")) {
+          input.value = eval(input.value);
+        } else {
+          const modifiedExpression = input.value.replace(
+            /(\d+)%/g,
+            "($1 / 100) *"
+          );
+          input.value = eval(modifiedExpression);
         }
-    });
+      } catch (error) {
+        input.value = "Error";
+      }
+    }
+  });
 });
-
